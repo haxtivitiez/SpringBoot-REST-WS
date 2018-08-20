6a7679fe0ac443540f19850c8090619ad8de872e
Anuwk <un4ckn0wl3z@gmail.com>
2018-08-20 21:32:24 +0700

    init

---
+++ src/main/java/com/anuwk/rest/ws/springbootrestws/SpringbootRestWsApplication.java
@@ -0,0 +1,12 @@
+package com.anuwk.rest.ws.springbootrestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestWsApplication.class, args);
+	}
+}
