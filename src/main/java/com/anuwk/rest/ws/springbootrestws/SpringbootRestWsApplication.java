40e3f18ab830a04e8e41c92f5f264c8bb72a8e98
Anuwk <un4ckn0wl3z@gmail.com>
2018-08-26 13:09:05 +0700

    implement api document using Swagger2

--- src/main/java/com/anuwk/rest/ws/springbootrestws/SpringbootRestWsApplication.java
+++ src/main/java/com/anuwk/rest/ws/springbootrestws/SpringbootRestWsApplication.java
@@ -1,12 +1,32 @@
 package com.anuwk.rest.ws.springbootrestws;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class SpringbootRestWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestWsApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	
+	@Bean(name="messageSource")
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 }
--- src/main/java/com/anuwk/rest/ws/springbootrestws/helloworld/HelloWorldController.java
+++ src/main/java/com/anuwk/rest/ws/springbootrestws/helloworld/HelloWorldController.java
@@ -1,14 +1,20 @@
 package com.anuwk.rest.ws.springbootrestws.helloworld;
 
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
@@ -23,6 +29,11 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-var/{name}")
 	public HelloWorldBean helloWorldPathVar(@PathVariable String name) {
 		return new HelloWorldBean("Hello World, " + name);
+	}
+	
+	@GetMapping(path="/hello-world-inter")
+	public String helloWorldInter(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+		return messageSource.getMessage("gm.message",null ,locale);
 	}
 
 
